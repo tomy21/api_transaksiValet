c1fbb811e2ea550907bb2c28d1e3a2f9602c4f61
DESKTOP-MIFG50S\Lenovo <tomy21.agung@gmail.com>
2024-03-04 01:45:31 +0700

    update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -67,11 +67,15 @@ function generateTransactionCodeValet(totalRow) {
   const year = currentDate.getFullYear();
   const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
   const day = ("0" + currentDate.getDate()).slice(-2);
-  const dateCode = year + month + day;
+  const hours = ("0" + currentDate.getHours()).slice(-2);
+  const minutes = ("0" + currentDate.getMinutes()).slice(-2);
+  const seconds = ("0" + currentDate.getSeconds()).slice(-2);
 
-  const randomCode = "VAL";
+  const dateCode = `${year}${month}${day}${hours}${minutes}${seconds}`;
 
-  const transactionCode = `${dateCode}${randomCode}${totalRow}`;
+  const paddedTotalRow = ("00000000" + totalRow).slice(-8); // Mengisi nol di depan
+
+  const transactionCode = `${dateCode}${paddedTotalRow}`;
 
   return transactionCode;
 }
