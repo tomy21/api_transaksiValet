eb0376038ae852af93246a65b3ebc9b025769567
DESKTOP-MIFG50S\Lenovo <tomy21.agung@gmail.com>
2024-03-06 11:44:34 +0700

    update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -6,7 +6,6 @@ const connection = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  // port: 3308,
   connectionLimit: 10,
   multipleStatements: true,
 });
