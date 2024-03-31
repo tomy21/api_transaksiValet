e06f9d8c73db6fa24a4a58cce6439af2c8218387
DESKTOP-MIFG50S\Lenovo <tomy21.agung@gmail.com>
2024-03-31 14:30:26 +0700

    codeUpdate

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -32,26 +32,26 @@ function generateUniqueTransactionCode(locationCode) {
   });
 }
 
-// //generate transaksi valet
-// function generateUniqueTransactionCodeValet() {
-//   return new Promise((resolve, reject) => {
-//     const countQuery =
-//       "SELECT COUNT(1) AS total_rows FROM TransactionParkingValet";
+//generate transaksi valet
+function generateUniqueTransactionCodeValet() {
+  return new Promise((resolve, reject) => {
+    const countQuery =
+      "SELECT COUNT(1) AS total_rows FROM TransactionParkingValet";
 
-//     connection.query(countQuery, (error, results) => {
-//       if (error) {
-//         reject(error);
-//         return;
-//       }
+    connection.query(countQuery, (error, results) => {
+      if (error) {
+        reject(error);
+        return;
+      }
 
-//       const totalRows = results[0].total_rows;
+      const totalRows = results[0].total_rows;
 
-//       const transactionCode = generateTransactionCodeValet(totalRows + 1);
+      const transactionCode = generateTransactionCodeValet(totalRows + 1);
 
-//       resolve(transactionCode);
-//     });
-//   });
-// }
+      resolve(transactionCode);
+    });
+  });
+}
 
 function generateTransactionCode(totalRow, locationCode) {
   const currentDate = new Date();
@@ -67,24 +67,24 @@ function generateTransactionCode(totalRow, locationCode) {
   return transactionCode;
 }
 
-// //Generate code valet
-// function generateTransactionCodeValet(totalRow) {
-//   const currentDate = new Date();
-//   const year = currentDate.getFullYear();
-//   const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
-//   const day = ("0" + currentDate.getDate()).slice(-2);
-//   const hours = ("0" + currentDate.getHours()).slice(-2);
-//   const minutes = ("0" + currentDate.getMinutes()).slice(-2);
-//   const seconds = ("0" + currentDate.getSeconds()).slice(-2);
+//Generate code valet
+function generateTransactionCodeValet(totalRow) {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+  const day = ("0" + currentDate.getDate()).slice(-2);
+  const hours = ("0" + currentDate.getHours()).slice(-2);
+  const minutes = ("0" + currentDate.getMinutes()).slice(-2);
+  const seconds = ("0" + currentDate.getSeconds()).slice(-2);
 
-//   const dateCode = `${year}${month}${day}${hours}${minutes}${seconds}`;
+  const dateCode = `${year}${month}${day}${hours}${minutes}${seconds}`;
 
-//   const paddedTotalRow = ("00000000" + totalRow).slice(-8); // Mengisi nol di depan
+  const paddedTotalRow = ("00000000" + totalRow).slice(-8); // Mengisi nol di depan
 
-//   const transactionCode = `${dateCode}${paddedTotalRow}`;
+  const transactionCode = `${dateCode}${paddedTotalRow}`;
 
-//   return transactionCode;
-// }
+  return transactionCode;
+}
 
 function generateKeyNumber(locationCode) {
   return new Promise((resolve, reject) => {
@@ -120,5 +120,5 @@ module.exports = {
   generateUniqueTransactionCode,
   generateKeyNumber,
   // generateTransactionCode,
-  // generateUniqueTransactionCodeValet,
+  generateUniqueTransactionCodeValet,
 };
