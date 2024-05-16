c5d1493672b163e84db7982f729748abe8c8a15d
tomyItSky <tomy.agung@skyparking.co.id>
2024-05-16 15:27:49 +0700

    .

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,15 +2,10 @@ import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    logging: false,
-  }
-);
+const db = new Sequelize("skybillingdb", "root", "50p4y5ky0v0!", {
+  host: "8.215.44.147",
+  dialect: "mysql",
+  logging: false,
+});
 
 export default db;
