33b1bfbdf0d939f5569a6d543011c0f5b19e76e4
tomyItSky <tomy.agung@skyparking.co.id>
2024-08-30 09:30:00 +0700

    .

--- controller/Users.js
+++ controller/Users.js
@@ -26,8 +26,8 @@ const createSendToken = (user, statusCode, res) => {
   const token = signToken(user);
 
   res.cookie("refreshToken", token, {
-    httpOnly: false,
-    secure: false,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
     expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
     sameSite: "None",
   });
